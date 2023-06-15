8c9a01840b74c8f566e6f1021a52f48d70d3f1b0
mcbraunie <mcbraunie@gmail.com>
2023-06-15 14:30:30 -0700

    added word scraping functionality

---
+++ helpers/fileReader.js
@@ -0,0 +1,27 @@
+/*
+ MB
+ 06/15/23
+ Purpose: to converty CSV file elements into array
+ for use by the word-guessing game
+ File: ./helpers/fileReader.js
+*/
+
+const fs = require('fs');
+
+/*
+Takes in a CSV file's contents (csvFP) and maps
+each value as an element within a new array
+*/
+function readFileContents(fp) {
+  const fileContent = fs.readFileSync(fp, 'utf-8');
+  const rows = fileContent.split('\n');
+  const csvData = rows.map(row => row.split(','));
+  return csvData;
+}
+
+// location of CSV file
+// const csvFP = './random.csv';
+// creating a new array to hold contents
+// const contents = readFileContents(csvFP);
+// print new array
+// console.log(contents);
---
+++ helpers/wordScraper.js
@@ -0,0 +1,39 @@
+/*
+ * Name: MB
+ * File: ./helpers/wordScraper.js
+ */
+
+// code to scrape words from URL
+// using puppeteer elements
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+// async function to get DOM elements
+(async () => {
+    // variable to store browser options
+    const launchOptions = { headless: true, }
+    // variable to store browser element
+    const browser = await puppeteer.launch(launchOptions);
+    // variable to store new page element
+    const page = await browser.newPage();
+    // go to URL with random words, wait until DOM is loaded
+    await page.goto('https://www.randomlists.com/random-words?dup=false&qty=100', {
+        waitUntil: 'domcontentloaded',});
+    let resultRandomWords = await page.$$eval('.rand_large', words => words.map(word => word.textContent));
+    arrayToCSV(resultRandomWords);
+    // wordArr = wordArr.concat(resultRandomWords);
+    await browser.close();
+})();
+
+// pushes data from URL-to-csv
+function arrayToCSV(arr){
+    // const fs = require('fs');
+    const writeToFile = arr;
+    const writeStream = fs.createWriteStream('random.csv');
+    writeStream.write(writeToFile.join(','));
+    writeStream.close();
+};
+
+
+
+
